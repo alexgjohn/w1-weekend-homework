80fb70f07d1b3c55bf828adcd81308fe4db2c515
Alex <alexgjohn@outlook.com>
2023-04-07 14:18:39 +0100

    completed non optional tests

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -37,5 +37,39 @@ def remove_pet_by_name(pet_shop, pet_name):
         if pet["name"] == pet_name:
             pet_shop["pets"].remove(pet)
 
+# def add_pet_to_stock(pet_shop, new_pet):
+#     new_pet = {}
+#     pet_shop["pets"].append(new_pet)
+#     return len(pet_shop["pets"])
+#this test passed BUT wouldn't add any information to the new dictionary
+#below is an example of a test that I THINK would fulfil this secondary function
+
+def add_pet_to_stock(pet_shop, new_pet_name):
+    new_pet = {"name" : str(new_pet_name)}
+    pet_shop["pets"].append(new_pet)
+    return len(pet_shop["pets"])
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+    return customer["cash"]
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+# def add_pet_to_customer(customer, new_pet):
+#     customer["pets"].append(new_pet)
+#     return len(customer["pets"])
+
+#same as above but should add this new pet as a dictionary so it can hold more data
+def add_pet_to_customer(customer, new_pet_name):
+    new_pet = {"name" : str(new_pet_name)}
+    customer["pets"].append(new_pet)
+    return len(customer["pets"])
+#only problem with this one is that it doesn't remove this pet from the pet shop, as it presumably has been taken from there.
+
+
 
 
